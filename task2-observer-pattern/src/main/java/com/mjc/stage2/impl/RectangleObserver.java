bede3cd75c55019652f08ebfcb64b56e97cb3814
Artsiom Pashun <artyom.pashun@gmail.com>
2023-08-10 10:27:49 +0200

    Solved observer design pattern task

--- task2-observer-pattern/src/main/java/com/mjc/stage2/impl/RectangleObserver.java
+++ task2-observer-pattern/src/main/java/com/mjc/stage2/impl/RectangleObserver.java
@@ -1,5 +1,27 @@
 package com.mjc.stage2.impl;
 
-public class RectangleObserver  {
-    // Write your code here!
+import com.mjc.stage2.Observer;
+import com.mjc.stage2.entity.Rectangle;
+import com.mjc.stage2.entity.RectangleValues;
+import com.mjc.stage2.event.RectangleEvent;
+import com.mjc.stage2.warehouse.RectangleWarehouse;
+
+public class RectangleObserver implements Observer {
+
+    RectangleWarehouse rectangleWarehouse = RectangleWarehouse.getInstance();
+
+    @Override
+    public void handleEvent(RectangleEvent event) {
+        Rectangle rectangle = event.getSource();
+        rectangleWarehouse.put(rectangle.getId(), new RectangleValues(findRectanglePerimeter(rectangle), findRectangleSquare(rectangle)));
+
+    }
+
+    public double findRectanglePerimeter(Rectangle rectangle) {
+        return (rectangle.getSideA() + rectangle.getSideB()) * 2;
+    }
+
+    public double findRectangleSquare(Rectangle rectangle) {
+        return rectangle.getSideA() * rectangle.getSideA();
+    }
 }
